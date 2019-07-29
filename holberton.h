169a978c0ced411a434c63efc5795ca37b3991d3
Carlos Zuluaga <carlosez9@hotmail.com>
2019-07-29 20:31:13 +0000

    Add print_str and update header file

--- holberton.h
+++ holberton.h
@@ -8,5 +8,9 @@ int _print_string(char *s);
 int _print_number(int n, int count);
 int _printf(const char *format, ...);
 int _print_selector(char *, va_list);
+int _special_chars(char *);
+int _print_percentage(char *);
+int _print_str(char *);
+
 
 #endif /* HOLBERTON H */
