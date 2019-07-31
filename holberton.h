5864c82bd20d70d195bc59fc935b3944486b2613
SantiagoHerreG <888@holbertonschool.com>
2019-07-31 19:46:09 +0000

    New update to the whole project, handling of buffer in memory so function write is called minimally

--- holberton.h
+++ holberton.h
@@ -3,20 +3,20 @@
 
 #include <stdarg.h>
 
-int _special_chars(char *str, char *buffer);
-int _write_char(char c, char *buffer);
+int _special_chars(int *p, char *str, char *buffer, int count);
+int _write_char(int *p, char c, char *buffer, int count);
 void _print_string(char *s, int count);
-int _print_number(int n, int count, char *buffer);
+int _print_number(int *p, int n, int count, char *buffer);
 int _printf(const char *format, ...);
 int _print_selector(char *, va_list, char *buffer);
-int _print_percentage(char *str, char *buffer);
-int _print_str(char *str, char *buffer);
-int _print_binary(unsigned int n, int count, char *buffer);
-int _print_octal(unsigned int n, int count, char *buffer);
-int _string_to_buff(char *str, char *buffer);
-int _print_hex(char, unsigned int, char *);
-int _print_unsigned_int(unsigned int n, int count, char *buffer);
-int _print_rev(char *str, char *buffer);
-int _print_rot13(char *str, char *buffer);
+int _print_percentage(int *p, char *str, char *buffer, int count);
+int _print_str(int *p, char *str, char *buffer, int count);
+int _print_binary(int *p, unsigned int n, int count, char *buffer);
+int _print_octal(int *p, unsigned int n, int count, char *buffer);
+int _string_to_buff(int *p, char *str, char *buffer, int count);
+int _print_h(int *p, char option, unsigned int n, char *buffer, int count);
+int _print_unsigned_int(int *p, unsigned int n, int count, char *buffer);
+int _print_rev(int *p, char *str, char *buffer, int count);
+int _print_rot13(int *p, char *str, char *buffer, int count);
 
 #endif /* HOLBERTON H */
--- print_number.c
+++ print_number.c
@@ -2,19 +2,19 @@
 /**
  * _print_number - prints an integer as if it were a string
  * @n: integer to print
+ * @p: pointer to how many times the buffer was printed
  * @count: Counter for number of characters in the integer
  * @buffer: buffer allocated in the heap
  * Return: Counter for number of characters in the integer
  */
-int _print_number(int n, int count, char *buffer)
+int _print_number(int *p, int n, int count, char *buffer)
 {
 	unsigned int k;
 
 	if (n < 0)
 	{
 		k = -n;
-		_write_char(45, buffer);
-		count++;
+		count = _write_char(p, 45, buffer, count);
 	}
 	else
 	{
@@ -22,26 +22,26 @@ int _print_number(int n, int count, char *buffer)
 	}
 	if (k / 10)
 	{
-		count = _print_number(k / 10, count, buffer);
+		count = _print_number(p, k / 10, count, buffer);
 	}
-	_write_char(k % 10 + '0', buffer + count);
+	count = _write_char(p, k % 10 + '0', buffer, count);
 
-	return (1 + count);
+	return (count);
 }
-
 
 /**
  * _print_unsigned_int - prints an integer as if it were a string
  * @n: unsigned integer to print
  * @count: Counter for number of characters in the unsigned integer
+ * @p: pointer to how many times buffer was filled
  * @buffer: Buffer array
  *
  * Return: Counter for number of characters in the unsigned integer
  */
-int _print_unsigned_int(unsigned int n, int count, char *buffer)
+int _print_unsigned_int(int *p, unsigned int n, int count, char *buffer)
 {
 	if (n / 10)
-		count = _print_unsigned_int(n / 10, count, buffer);
-	buffer[count] = n % 10 + '0';
-	return (1 + count);
+		count = _print_unsigned_int(p, n / 10, count, buffer);
+	count = _write_char(p, n % 10 + '0', buffer, count);
+	return (count);
 }
